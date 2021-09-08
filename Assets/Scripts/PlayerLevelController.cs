8813e7dc5f480831f25fb9a874f7993a0408d8de
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2021-09-08 13:45:44 +0900

    経験値処理手抜き

---
+++ Assets/Scripts/PlayerLevelController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class PlayerLevelController : ScriptableObject
+{
+    [SerializeField] int[] m_level;
+    [SerializeField] int[] m_maxHP;
+    [SerializeField] int[] m_maxMP;
+    [SerializeField] int[] m_agility;
+    [SerializeField] int[] m_defence;
+}
