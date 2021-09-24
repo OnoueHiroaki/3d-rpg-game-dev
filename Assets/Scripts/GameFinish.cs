c88bda9a46f5a0d88ad3a164097a9e31c1a31bd7
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2021-09-24 18:30:48 +0900

    ゲーム終了ボタン追加

---
+++ Assets/Scripts/GameFinish.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFinish : MonoBehaviour
+{
+    public void OnClickFinish()
+    {
+        //UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
