f1e159211eb54fd17727d0c9cc7007d0d09421b2
OnoueHiroaki <hirophon18@ezweb.ne.jp>
2021-09-24 15:58:51 +0900

    マウスカーソル修正

---
+++ Assets/Scripts/UI/CommandBattleText.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommandBattleText : MonoBehaviour
+{
+    [SerializeField] Text m_winText;
+    [SerializeField] Text m_hPText;
+    [SerializeField] Text m_mPText;
+
+    public Text WinText { get => m_winText; }
+    public Text HPText { get => m_hPText; }
+    public Text MPText { get => m_mPText; }
+}
